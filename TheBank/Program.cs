161d302ea7a8a3f5952226ae3b648aca8fb4bdc6
REKTorn <emanuel.carlhed@gmail.com>
2020-10-07 09:24:51 +0200

    Started Commenting

--- TheBank/Program.cs
+++ TheBank/Program.cs
@@ -13,50 +13,51 @@ namespace TheBank
         static List<Customer> customers = new List<Customer>();
         static void Main(string[] args)
         {
+            // Starts the program with this welcoming message.
             Console.WriteLine("Välkommen till banken!");
             Console.WriteLine();
             Console.WriteLine("Ange ett alternativ.");
             Console.WriteLine();
 
             int choice = 0;
-            while ( choice != 7 )
+            while ( choice != 7 ) // Looping the menu switch as long as the entered value is not "7".
             {
-                choice = SelectMenuItem();
+                choice = SelectMenuItem(); // Calls and puts the function "SelectMenuItem()"s return value into the variable "choise".
 
-                switch (choice)
+                switch (choice) // Starts the switch with the variable "choise" as a "key".
                 {
-                    case 1:
-                        AddCustomer(); 
+                    case 1: // Case one will make the user add a customer to the customers list,
+                        AddCustomer(); // with this function.
                         break;
 
-                    case 2:
-                        RemoveCustomer();
+                    case 2: // Case 2 will make the user remove a customer from the customers list,
+                        RemoveCustomer(); // with this function.
                         break;
 
-                    case 3:
-                        ShowAllCustomers();
-                        Console.WriteLine();
+                    case 3: // Case 3 will display all the current customers existing,
+                        ShowAllCustomers(); // with this function.
                         break;
 
-                    case 4:
-                        ShowBalance();
+                    case 4: // Case 4 will make the user choose a customer which balance will be displayed,
+                        ShowBalance(); // with this function.
                         break;
 
-                    case 5:
-                        Deposit();
+                    case 5: // Case 5 will make a deposit into a chosen customers bank balance,
+                        Deposit(); // with this function.
                         break;
 
-                    case 6:
-                        Withdraw();
+                    case 6: // Case 6 will make a withdrawal from a chosen customers bank balance,
+                        Withdraw(); // with this function.
                         break;
 
-                    case 7:
+                    case 7: // Case 7, wich is when the loops break, the program will not continue anymore.
+                        // And some farewell messages are being printed.
                         Console.WriteLine("Välkommen åter!");
                         Console.WriteLine("Tryck enter för att stänga fönstret...");
                         break;
 
-                    default:
-                        Console.WriteLine("Ogiltigt svar.");
+                    default: // The default will run in case of a value that do not match the other cases.
+                        Console.WriteLine("Ogiltigt svar."); // Printing an error message.
                         Console.WriteLine();
                         break;
                 }
@@ -93,13 +94,13 @@ namespace TheBank
 
         private static void Deposit()
         {
-            ShowAllCustomers();
-            Console.WriteLine("Välj en användare att göra insättning på.");
-            int i = int.Parse(Console.ReadLine());
+            ShowAllCustomers(); // Firsly runs a function to display all the customers.
+            Console.WriteLine("Välj en användare att göra insättning på."); // Asks to chose a customer.
+            int i = int.Parse(Console.ReadLine()); // Adds the chosen customer to an integer variable.
             Console.WriteLine();
-            Console.WriteLine("Hur mycket vill du sätta in?");
-            int addBalance = int.Parse(Console.ReadLine());
-            customers[i].Balance = customers[i].Balance + addBalance;
+            Console.WriteLine("Hur mycket vill du sätta in?"); // Asks how much the user wants to deposit.
+            int addBalance = int.Parse(Console.ReadLine()); // Adds the value into a variable.
+            customers[i].Balance = customers[i].Balance + addBalance; // The chosen customers balance is added with the deposit value.
             Console.WriteLine();
         }
 
@@ -149,6 +150,8 @@ namespace TheBank
             {
                 Console.WriteLine(Name.ShowCustomer);
             }*/
+
+            Console.WriteLine();
         }
 
         private static int SelectMenuItem()
